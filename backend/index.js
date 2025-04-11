b328df2d11c8a1639030777bafebc3d37efc176d
Abhishek shivale <abhishekshivale@Abhisheks-MacBook-Air.local>
2025-04-11 07:05:05 +0530

    added cors url

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['*', 'http://localhost:5173']
+  origin: ['*', 'http://localhost:5173', 'https://assignment-cuvette-two.vercel.app']
 }));
 app.use(express.json());
 
